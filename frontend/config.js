02ccdde2414eb834033379535a539ce0a08aca8c
mark eric <streamhub619@gmail.com>
2026-07-31 20:08:23 +0300

    added /api to backend url

--- frontend/config.js
+++ frontend/config.js
@@ -2,5 +2,5 @@ const CONFIG = {
   API_BASE: window.location.hostname === 'localhost' || 
             window.location.hostname === '127.0.0.1'
     ? 'http://localhost:5000/api'
-    : 'https://mockbot-yil5.onrender.com',
+    : 'https://mockbot-yil5.onrender.com/api',
 };
